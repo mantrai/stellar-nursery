f6fe1a1df01da18067596bd7c3c727a91ceaac97
mantrai <mantra@penitence.eu>
2021-02-11 18:11:17 +0000

    Added generic publisher

---
+++ src/stellar-nursery-publisher.ts
@@ -0,0 +1,31 @@
+import IPublisher from "./interfaces/i-publisher";
+import ISubscriber from "./interfaces/i-subscriber";
+
+export default class StellarNurseryPublisher<K, O, R> implements IPublisher<K, O, R>{
+    private _subscriptions: Map<K, ISubscriber<K, O, R>> = new Map<K, ISubscriber<K, O, R>>();
+    bulkSubscribe(subscribers: Map<K, ISubscriber<K, O, R>>): IPublisher<K, O, R> {
+        this._subscriptions = subscribers;
+        return this;
+    }
+
+    getSubscribers(): Map<K, ISubscriber<K, O, R>> {
+        return this._subscriptions;
+    }
+
+    getSubscription(key: K): ISubscriber<K, O, R> | false {
+        return this.hasSubscription(key) ? this._subscriptions.get(key) as ISubscriber<K, O, R> : false;
+    }
+
+    hasSubscription(key: K): boolean {
+        return this._subscriptions.has(key);
+    }
+
+    subscribe(subscriber: ISubscriber<K, O, R>): IPublisher<K, O, R> {
+        this._subscriptions.set(subscriber.getKey(), subscriber);
+        return this;
+    }
+
+    getKeys(): K[] {
+        return Array.from(this._subscriptions.keys()).sort() as K[];
+    }
+}
