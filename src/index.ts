8d74d2f92531b6d6faaadafc213bd3ed0aad6408
mantrai <mantra@penitence.eu>
2021-02-20 23:28:22 +0000

    Removed  fast-safe-stringify as json output is working fine

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-import safeStringify from 'fast-safe-stringify';
 import StellaNurseryFactory from './stella-nursery-factory';
 
 const factory = new StellaNurseryFactory('11896589443320660');
-console.log(safeStringify(factory.run()));
+console.log(JSON.stringify(factory.run()));
